38897b56f227cd12c68537ed7cc48a9371a90003
Alfie-King <alfie.king@theinformationlab.co.uk>
2025-11-25 14:22:43 +0000

    adding documentation to the extract.py script

--- extract.py
+++ extract.py
@@ -7,13 +7,18 @@ import os
 import mailchimp_marketing as MailchimpMarketing
 from mailchimp_marketing.api_client import ApiClientError
 
+# Adding extract function so that the code can be run from 1 script
 def extract(start_date, before_date): 
 
+  # Loading the .env data to keep private
   load_dotenv()
   API_KEY = os.getenv('API_KEY')
   SERVER_PREFIX = os.getenv('SERVER_PREFIX')
+
+  # Creating extract timestamp so that it can be seen when the data was extracted
   extract_timestamp = dt.now().strftime('%Y-%m-%dT%H-%M-%S')
 
+  # Testing connection to API
   try:
     client = MailchimpMarketing.Client()
     client.set_config({
@@ -30,9 +35,13 @@ def extract(start_date, before_date):
     )
 
     print('Connection Success')
+
+    # Downloads the data from the API to the data folder to then be put into S3 by the load function
+    filepath = 'data/mailchimp_campaigns_' + extract_timestamp + '.json'
+    with open(filepath,'w') as file:
+        json.dump(response,file)
+
+  # If connection to the API is bad it outputs this error        
   except ApiClientError as error:
     print("Error: {}".format(error.text))
 
-  filepath = 'data/mailchimp_campaigns_' + extract_timestamp + '.json'
-  with open(filepath,'w') as file:
-      json.dump(response,file)
